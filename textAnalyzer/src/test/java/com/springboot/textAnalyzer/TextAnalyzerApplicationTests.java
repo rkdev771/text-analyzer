44b87e3c24b32ee040a2e465e9de4012a5814276
rk.dev771 <rk.dev771@gmail.com>
2026-03-11 23:52:53 -0400

    Initial Spring Boot setup

---
+++ textAnalyzer/src/test/java/com/springboot/textAnalyzer/TextAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.textAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
