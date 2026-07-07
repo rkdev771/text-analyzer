74246f703d4b088620db8d8ac1c6917322e77d62
rk.dev771 <rk.dev771@gmail.com>
2026-07-07 01:58:41 -0400

    added health check endpoint

---
+++ backend/src/main/java/com/springboot/textAnalyzer/controllers/HealthController.java
@@ -0,0 +1,13 @@
+package com.springboot.textAnalyzer.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/healthz")
+    public String healthCheck() {
+        return "OK";
+    }
+}
